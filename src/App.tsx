9566fe4d8d896b3efe134e5a6e1f31b75396444c
shanuagrawal <agrawalshanu798@gmail.com>
2023-02-06 22:20:30 +0530

    set up done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './app.scss';
 
 function App() {
+
+const styles = {
+container : 'cl__app__container',
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+    <h1> Component library coming soon </h1>
     </div>
   );
 }
